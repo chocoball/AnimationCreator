2308b9c50a0c0035400e873b545b0d4cbbf8f6b4
chocoball <shin_zepherx@hotmail.com>
2012-01-11 19:04:11 +0900

    マックのコマンドライン
    引数とれてなかったのを修正

--- src/capplication.cpp
+++ src/capplication.cpp
@@ -7,147 +7,150 @@
 #include "canm2d.h"
 
 CApplication::CApplication(int argc, char **argv) :
-	QApplication(argc, argv),
-	m_execType(kExecType_Normal)
+    QApplication(argc, argv),
+    m_execType(kExecType_Normal)
 {
+    for ( int i = 0 ; i < argc ; i ++ ) {
+        m_args.append(argv[i]);
+    }
 #if 0
-	AllocConsole();
-	freopen("CONOUT$", "w", stdout);
-	freopen("CONIN$", "r", stdin);
+    AllocConsole();
+    freopen("CONOUT$", "w", stdout);
+    freopen("CONIN$", "r", stdin);
 #endif
 }
 
 bool CApplication::checkOpt()
 {
-	bool bNextInput = false ;
-	bool bNextOutput = false ;
-	for ( int i = 1 ; i < arguments().size() ; i ++ ) {
-		const QString str = arguments().at(i) ;
-		if ( str == "-h" || str == "--help" ) {
-			return false ;
-		}
-		else if ( str == "-asm" ) {
-			m_execType = kExecType_OutputAsm ;
-		}
-		else if ( str == "-i" ) {
-			bNextInput = true ;
-		}
-		else if ( str == "-o" ) {
-			bNextOutput = true ;
-		}
-		else {
-			if ( bNextInput ) {
-				m_inputFile = arguments().at(i) ;
-				m_inputFile = m_inputFile.replace("\\", "/") ;
-				bNextInput = false ;
-			}
-			else if ( bNextOutput ) {
-				m_outputFile = arguments().at(i) ;
-				m_outputFile = m_outputFile.replace("\\", "/") ;
-				bNextOutput = false ;
-			}
-		}
-	}
+    bool bNextInput = false ;
+    bool bNextOutput = false ;
+    for ( int i = 1 ; i < m_args.size() ; i ++ ) {
+        const QString str = m_args.at(i) ;
+        if ( str == "-h" || str == "--help" ) {
+            return false ;
+        }
+        else if ( str == "-asm" ) {
+            m_execType = kExecType_OutputAsm ;
+        }
+        else if ( str == "-i" ) {
+            bNextInput = true ;
+        }
+        else if ( str == "-o" ) {
+            bNextOutput = true ;
+        }
+        else {
+            if ( bNextInput ) {
+                m_inputFile = m_args.at(i) ;
+                m_inputFile = m_inputFile.replace("\\", "/") ;
+                bNextInput = false ;
+            }
+            else if ( bNextOutput ) {
+                m_outputFile = m_args.at(i) ;
+                m_outputFile = m_outputFile.replace("\\", "/") ;
+                bNextOutput = false ;
+            }
+        }
+    }
 
-	switch ( m_execType ) {
-		case kExecType_Normal:
-			if ( arguments().size() > 1 ) {
-				m_execType = kExecType_ArgFile ;
-				m_inputFile = arguments().at(1) ;
-				m_inputFile = m_inputFile.replace("\\", "/") ;
-			}
-			break ;
-		case kExecType_OutputAsm:
-			if ( m_inputFile.isEmpty() || m_outputFile.isEmpty() ) { return false ; }
-			break ;
-	}
+    switch ( m_execType ) {
+        case kExecType_Normal:
+            if ( m_args.size() > 1 ) {
+                m_execType = kExecType_ArgFile ;
+                m_inputFile = m_args.at(1) ;
+                m_inputFile = m_inputFile.replace("\\", "/") ;
+            }
+            break ;
+        case kExecType_OutputAsm:
+            if ( m_inputFile.isEmpty() || m_outputFile.isEmpty() ) { return false ; }
+            break ;
+    }
 
-	return true ;
+    return true ;
 }
 
 void CApplication::print_usage()
 {
-	qDebug() << trUtf8("usage : asm吐き出し") ;
-	qDebug() << trUtf8("        %1 -asm -i input_file -o output_file").arg(this->applicationName()) ;
+    qDebug() << trUtf8("usage : asm吐き出し") ;
+    qDebug() << trUtf8("        %1 -asm -i input_file -o output_file").arg(this->applicationName()) ;
 }
 
 int CApplication::execute()
 {
-	switch ( m_execType ) {
-		case kExecType_Normal:
-		case kExecType_ArgFile:
-			{
-				Q_INIT_RESOURCE(Resource) ;
-				QTranslator translator;
-				translator.load(":/root/Resources/lang/linguist_ja.qm");
-				installTranslator(&translator) ;
+    switch ( m_execType ) {
+        case kExecType_Normal:
+        case kExecType_ArgFile:
+            {
+                Q_INIT_RESOURCE(Resource) ;
+                QTranslator translator;
+                translator.load(":/root/Resources/lang/linguist_ja.qm");
+                installTranslator(&translator) ;
 
-				MainWindow w;
-				w.show();
+                MainWindow w;
+                w.show();
 
-				if ( !m_inputFile.isEmpty() ) {
-					w.fileOpen(m_inputFile) ;
-				}
+                if ( !m_inputFile.isEmpty() ) {
+                    w.fileOpen(m_inputFile) ;
+                }
 
-				return this->exec() ;
-			}
-		case kExecType_OutputAsm:
-			{
-				CEditData editData ;
-				{	// 入力ファイル読み込み
-					CAnm2DXml data(false) ;
-					QFile file(m_inputFile) ;
-					if ( !file.open(QFile::ReadOnly) ) {
-						qDebug() << trUtf8("ファイルオープン失敗[") << m_inputFile << "]" ;
-						print_usage();
-						return 1 ;
-					}
-					QDomDocument xml ;
-					xml.setContent(&file) ;
-					data.setFilePath(m_inputFile);
-					if ( !data.makeFromFile(xml, editData) ) {
-						qDebug() << trUtf8("ファイル読み込み失敗[") << m_inputFile << "]:" << data.getErrorString() ;
-						print_usage();
-						return 1 ;
-					}
-				}
-				{	// 出力ファイル書き込み
-					CAnm2DAsm	data(false) ;
-					if ( !data.makeFromEditData(editData) ) {
-						if ( data.getErrorNo() != CAnm2DBase::kErrorNo_Cancel ) {
-							qDebug() << trUtf8("コンバート失敗[") << m_outputFile << "]:" << data.getErrorString() ;
-							print_usage();
-						}
-						return 1 ;
-					}
+                return this->exec() ;
+            }
+        case kExecType_OutputAsm:
+            {
+                CEditData editData ;
+                {	// 入力ファイル読み込み
+                    CAnm2DXml data(false) ;
+                    QFile file(m_inputFile) ;
+                    if ( !file.open(QFile::ReadOnly) ) {
+                        qDebug() << trUtf8("ファイルオープン失敗[") << m_inputFile << "]" ;
+                        print_usage();
+                        return 1 ;
+                    }
+                    QDomDocument xml ;
+                    xml.setContent(&file) ;
+                    data.setFilePath(m_inputFile);
+                    if ( !data.makeFromFile(xml, editData) ) {
+                        qDebug() << trUtf8("ファイル読み込み失敗[") << m_inputFile << "]:" << data.getErrorString() ;
+                        print_usage();
+                        return 1 ;
+                    }
+                }
+                {	// 出力ファイル書き込み
+                    CAnm2DAsm	data(false) ;
+                    if ( !data.makeFromEditData(editData) ) {
+                        if ( data.getErrorNo() != CAnm2DBase::kErrorNo_Cancel ) {
+                            qDebug() << trUtf8("コンバート失敗[") << m_outputFile << "]:" << data.getErrorString() ;
+                            print_usage();
+                        }
+                        return 1 ;
+                    }
 
-					QFile file(m_outputFile) ;
-					if ( !file.open(QFile::WriteOnly) ) {
-						qDebug() << trUtf8("ファイル書き込み失敗[") << m_outputFile << "]:" << data.getErrorString() ;
-						print_usage();
-						return 1 ;
-					}
-					file.write(data.getData().toAscii()) ;
+                    QFile file(m_outputFile) ;
+                    if ( !file.open(QFile::WriteOnly) ) {
+                        qDebug() << trUtf8("ファイル書き込み失敗[") << m_outputFile << "]:" << data.getErrorString() ;
+                        print_usage();
+                        return 1 ;
+                    }
+                    file.write(data.getData().toAscii()) ;
 
-					QString		incFileName = m_inputFile;
-					incFileName.replace(QString(".asm"), QString(".inc"));
-					CAnm2DAsm	dataInc(false);
-					if(!dataInc.makeFromEditData2Inc(editData)){
-						if(dataInc.getErrorNo() != CAnm2DBase::kErrorNo_Cancel){
-							qDebug() << trUtf8("コンバート失敗[") << incFileName << "]:" << data.getErrorString() ;
-							print_usage();
-						}
-						return 1 ;
-					}
-					QFile	fileInc(incFileName);
-					if(!fileInc.open(QFile::WriteOnly)){
-						qDebug() << trUtf8("ファイル書き込み失敗[") << incFileName << "]:" << data.getErrorString() ;
-						return 1 ;
-					}
-					fileInc.write(dataInc.getData().toAscii());
-				}
-			}
-			break ;
-	}
-	return 0 ;
+                    QString		incFileName = m_outputFile;
+                    incFileName.replace(QString(".asm"), QString(".inc"));
+                    CAnm2DAsm	dataInc(false);
+                    if(!dataInc.makeFromEditData2Inc(editData)){
+                        if(dataInc.getErrorNo() != CAnm2DBase::kErrorNo_Cancel){
+                            qDebug() << trUtf8("コンバート失敗[") << incFileName << "]:" << data.getErrorString() ;
+                            print_usage();
+                        }
+                        return 1 ;
+                    }
+                    QFile	fileInc(incFileName);
+                    if(!fileInc.open(QFile::WriteOnly)){
+                        qDebug() << trUtf8("ファイル書き込み失敗[") << incFileName << "]:" << data.getErrorString() ;
+                        return 1 ;
+                    }
+                    fileInc.write(dataInc.getData().toAscii());
+                }
+            }
+            break ;
+    }
+    return 0 ;
 }
--- src/capplication.h
+++ src/capplication.h
@@ -2,35 +2,37 @@
 #define CAPPLICATION_H
 
 #include <QtGui/QApplication>
+#include <QStringList>
 
 enum {
-	kExecType_Normal = 0,	// 通常起動
-	kExecType_ArgFile,		// ファイル指定で起動
-	kExecType_OutputAsm		// 指定ファイルをasm形式に吐き出して終了
+    kExecType_Normal = 0,	// 通常起動
+    kExecType_ArgFile,		// ファイル指定で起動
+    kExecType_OutputAsm		// 指定ファイルをasm形式に吐き出して終了
 } ;
 
 class CApplication : public QApplication
 {
     Q_OBJECT
 public:
-	explicit CApplication(int argc, char **argv);
+    explicit CApplication(int argc, char **argv);
 
-	bool checkOpt() ;
-	void print_usage() ;
-	int execute() ;
+    bool checkOpt() ;
+    void print_usage() ;
+    int execute() ;
 
-	const int getExecType() { return m_execType ; }
-	const QString &getInputFile() { return m_inputFile ; }
-	const QString &getOutputFile() { return m_outputFile ; }
+    const int getExecType() { return m_execType ; }
+    const QString &getInputFile() { return m_inputFile ; }
+    const QString &getOutputFile() { return m_outputFile ; }
 
 signals:
 
 public slots:
 
 private:
-	int		m_execType ;
-	QString	m_inputFile ;
-	QString m_outputFile ;
+    int         m_execType ;
+    QString     m_inputFile ;
+    QString     m_outputFile ;
+    QStringList m_args ;
 };
 
 #endif // CAPPLICATION_H
